026dae0f4de7170b4f191788d3cf13c7d050a40f
Kayongo brian <kayonbrian@gmail.com>
2022-02-11 08:09:01 +0300

    starting the nav bar

---
+++ src/HomeScreen.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './HomeScreen.css';
+import Nav from './Nav';
+
+function HomeScreen() {
+  return (
+    <div>
+        {/* navbar */}
+        <Nav />
+
+        {/* banner */}
+
+        {/* Movie Rows */}
+    </div>
+  )
+}
+
+export default HomeScreen
---
+++ src/firebase.jsx
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC5K-2ogeaSHhnyjuMoqXGQA9C4XR5YS_Y",
+    authDomain: "netflix-clone-75eff.firebaseapp.com",
+    projectId: "netflix-clone-75eff",
+    storageBucket: "netflix-clone-75eff.appspot.com",
+    messagingSenderId: "674191988893",
+    appId: "1:674191988893:web:b95ff5b0586e7de6aa1c74",
+    measurementId: "G-K8WSK3ZZ66"
+  };
